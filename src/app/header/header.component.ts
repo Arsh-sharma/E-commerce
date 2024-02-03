00d99e421b1dd8de987ee5db3d30b6dbda6477be
Arsh Sharma <arsh705s10@gmail.com>
2024-02-03 16:34:30 +0530

    [Header-seller]

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,21 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit{
   constructor(private router: Router) {}
 
+  menuType: String = "default";
+
   ngOnInit(): void {
-      this.router.events.subscribe(()=>{})
+      this.router.events.subscribe((val:any)=>{
+        // console.log(val.url)
+
+        if(val.url){
+          console.log(val.url);
+          if(localStorage.getItem('seller') && val.url.includes('seller')){
+            console.log("in-seller-area");
+            this.menuType = "seller"
+          }else{
+            console.log("out-seller-area")
+          }
+        }
+      })
   }
 }
